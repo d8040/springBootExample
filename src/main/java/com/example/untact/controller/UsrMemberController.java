a6971f7173e781f2c79eb7db5360fbec95e61138
d8040 <d8040@nate.com>
2021-02-26 14:03:04 +0900

    회원가입 시 로그인아이디 중복체크

--- src/main/java/com/example/untact/controller/UsrMemberController.java
+++ src/main/java/com/example/untact/controller/UsrMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.untact.dto.Member;
 import com.example.untact.dto.ResultData;
 import com.example.untact.service.MemberService;
 
@@ -23,6 +24,12 @@ public class UsrMemberController {
 		
 		if (param.get("loginId") == null) {
 			return new ResultData("F-1", "loginId을 입력해주세요");
+		}
+		
+		Member existingMember = memberService.getMemberByLoginId((String) param.get("loginId"));
+		
+		if (existingMember != null) {
+		    return new ResultData ("F-2", String.format("%s (은)는 이미 사용중인 아이디 입니다.", param.get("loginId")));
 		}
 		
 		if (param.get("loginPw") == null) {
--- src/main/java/com/example/untact/service/MemberService.java
+++ src/main/java/com/example/untact/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.untact.dao.MemberDao;
+import com.example.untact.dto.Member;
 import com.example.untact.dto.ResultData;
 import com.example.untact.util.Util;
 
@@ -13,13 +14,21 @@ import com.example.untact.util.Util;
 public class MemberService {
     @Autowired
     private MemberDao memberDao;
-    
+
     public ResultData join(Map<String, Object> param) {
 	memberDao.join(param);
 
 	int id = Util.getAsInt(param.get("id"), 0);
 
 	return new ResultData("S-1", String.format("%s님 환영합니다.", param.get("nickname")), "id", id);
+    }
+
+    public Member getMemberByLoginId(String loginId) {
+	return memberDao.getMemberByLoginId(loginId);
+    }
+
+    public Member getMember(int id) {
+	return memberDao.getMember(id);
     }
 
 }
