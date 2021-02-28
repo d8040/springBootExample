66eb6b74229061cdb2665287446c1cf2538e1f72
d8040 <d8040@nate.com>
2021-02-28 11:43:47 +0900

    폼 테스트

--- src/main/java/com/example/untact/controller/UsrHomeController.java
+++ src/main/java/com/example/untact/controller/UsrHomeController.java
@@ -1,8 +1,12 @@
 package com.example.untact.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.untact.util.Util;
 
 @Controller
 public class UsrHomeController {
@@ -10,5 +14,11 @@ public class UsrHomeController {
 	@ResponseBody
 	public String showMain() {
 		return "이렇게 하면 되는건가??";
+	}
+	
+	@RequestMapping("/usr/home/doFormTest")
+	@ResponseBody
+	public Map<String, Object> doFormTest(String name, int age) {
+		return Util.mapOf("name", name, "age", age);
 	}
 }
